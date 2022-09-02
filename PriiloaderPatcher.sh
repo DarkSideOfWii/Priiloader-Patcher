0ba63612599470ee32962c727ef0a059c73f9cde
Joshua Faganello <112542668+DarkSideOfWii@users.noreply.github.com>
2022-09-02 21:41:57 +0200

    Add files via upload

---
+++ PriiloaderPatcher.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd Desktop/
+mkdir Priiloader
+cd Priiloader
+git clone https://github.com/DarkSideOfWii/Priiloader-Patcher.git
+echo "Thanks, now you can install Priiloader, an essential brik protection for your old Wii"
